2469d20d9869af0893757d731cd494a016d7f865
FIXO Queretaro <jorge.galan@fixo.mx>
2026-08-24 15:12:24 -0600

    Configura el dominio público de Heredom

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
-  site: "https://heredom.example",
+  site: "https://heredom.com.mx",
   trailingSlash: "always",
 });
